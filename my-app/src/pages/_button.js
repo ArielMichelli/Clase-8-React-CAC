2f114e76935c46566fd60a929a4ecf82978df0df
ArielMichelli <Arielm-04@hotmail.com>
2023-03-23 20:32:44 -0300

    Subiendo clase 8

---
+++ my-app/src/pages/_button.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Button = () => {
+    return (
+        <>
+            <button>Boton Prueba</button>
+            <div>
+                <h1>HOLA SEÑOR</h1>
+            </div>
+        </>
+    );
+};
